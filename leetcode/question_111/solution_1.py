a710bd26b0af6c2a12058aa1d4450a813d771b70
asxzhy <asxzhy.zhang@gmail.com>
2020-12-29 18:00:22 -0400

    Modified unchanged method name calling

--- leetcode/question_111/solution_1.py
+++ leetcode/question_111/solution_1.py
@@ -24,11 +24,11 @@ class Solution:
 
         # if the node only have a left node, proceed to the left node
         if not root.right:
-            return self.minDepth(root.left) + 1
+            return self.min_depth(root.left) + 1
 
         # if the node only have a right node, proceed to the right node
         if not root.left:
-            return self.minDepth(root.right) + 1
+            return self.min_depth(root.right) + 1
 
         # get the min depth between the left and right node
-        return min(self.minDepth(root.left), self.minDepth(root.right)) + 1
+        return min(self.min_depth(root.left), self.min_depth(root.right)) + 1
